e1ffbc17c4063cead3cdf9038d466c362f153999
vishu-tyagi <vishweshwartyagiv@gmail.com>
2023-02-21 18:43:44 +0000

    add data preprocessor

---
+++ src/hb-mep/hb_mep/utils/constants.py
@@ -0,0 +1,17 @@
+from hb_mep.config import HBMepConfig as config
+
+
+DATA_DIR = 'data'
+
+PARTICIPANT = 'participant'
+INTENSITY = 'intensity'
+MEP_SIZE = 'mep_size'
+SEGMENT = config.SEGMENT_FEATURE
+
+PARTICIPANT_ENCODER = 'participant_encoder'
+SEGMENT_ENCODER = 'segment_encoder'
+
+NUM_PARTICIPANTS = 'num_participants'
+NUM_SEGMENTS = 'num_segments'
+SEGMENTS_PER_PARTICIPANT = 'segments_per_participant'
+TOTAL_COMBINATIONS = 'total_combinations'
