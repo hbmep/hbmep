158f680e9680a973221f495ad2e4635ecd02c9dd
vishu-tyagi <vishweshwartyagiv@gmail.com>
2023-03-07 22:03:25 +0000

    add baseline with multivariate prior

--- src/hb-mep/hb_mep/models/mv_baseline.py
+++ src/hb-mep/hb_mep/models/mv_baseline.py
@@ -57,15 +57,22 @@ class MVBaseline():
         n_participant = np.unique(participant).shape[0]
         n_independent = np.unique(independent).shape[0]
 
-        with numpyro.plate("n_independent", n_independent, dim=-1):
-            a_level_mean = numpyro.sample("a_level_mean", dist.HalfNormal(a_level_mean_global_scale))
-            a_level_scale = numpyro.sample("a_level_scale", dist.HalfNormal(a_level_scale_global_scale))
+        a_level_mean = numpyro.sample(
+            "a_level_mean",
+            dist.MultivariateNormal(
+                a_level_scale_global_scale * jnp.ones(5), jnp.diag(jnp.ones(5))
+            )
+        )
+        a_level_scale = numpyro.sample(
+            "a_level_scale",
+            dist.MultivariateNormal(
+                a_level_mean_global_scale * jnp.ones(5), jnp.diag(jnp.ones(5))
+            )
+        )
 
+        with numpyro.plate("n_independent", n_independent, dim=-1):
             b_level_mean = numpyro.sample("b_level_mean", dist.HalfNormal(b_level_mean_global_scale))
             b_level_scale = numpyro.sample("b_level_scale", dist.HalfNormal(b_level_scale_global_scale))
-
-            lo_level_mean = numpyro.sample("lo_level_mean", dist.HalfNormal(lo_level_mean_global_scale))
-            lo_level_scale = numpyro.sample("lo_level_scale", dist.HalfNormal(lo_level_scale_global_scale))
 
             sigma_offset_level_scale = \
                 numpyro.sample(
@@ -82,12 +89,10 @@ class MVBaseline():
                 a = numpyro.sample("a", dist.Normal(a_level_mean, a_level_scale))
                 b = numpyro.sample("b", dist.Normal(b_level_mean, b_level_scale))
 
-                lo = numpyro.sample("lo", dist.Normal(lo_level_mean, lo_level_scale))
-
                 sigma_offset = numpyro.sample('sigma_offset', dist.HalfCauchy(sigma_offset_level_scale))
                 sigma_slope = numpyro.sample('sigma_slope', dist.HalfCauchy(sigma_slope_level_scale))
 
-        mean = lo[participant, independent] + self.link(
+        mean = self.link(
             jnp.multiply(b[participant, independent], intensity - a[participant, independent])
         )
         sigma = sigma_offset[participant, independent] + sigma_slope[participant, independent] * mean
