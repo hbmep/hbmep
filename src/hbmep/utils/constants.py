917762c6f8256d1522bd3b0ea5af0a318fd69348
Vishu Tyagi <70053919+vishu-tyagi@users.noreply.github.com>
2023-07-14 11:14:50 -0400

    FIX: Fix error while parsing config file when optional MEP data is missing (#14)

--- src/hbmep/utils/constants.py
+++ src/hbmep/utils/constants.py
@@ -22,11 +22,10 @@ BASE = "base"
 MODEL = "model"
 LINK = "link"
 
-OPTIONAL = "optional"
-MEP_DATA = "mep_data"
+MEP_DATA = "optional-mep-data"
 MEP_MATRIX_PATH = "mep_matrix_path"
 MEP_TIME_RANGE = "mep_time_range"
-MEP_SIZE_WINDOW = "mep_size_window"
+MEP_SIZE_WINDOW = "optional-mep-size-window"
 MEP_SIZE_TIME_RANGE = "mep_size_time_range"
 
 RAW = "raw_"
