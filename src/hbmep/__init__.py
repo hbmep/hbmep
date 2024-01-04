dadf2f95c2feb1906356cd7aec2814114001dc6d
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-01-04 15:31:42 -0500

    fix: set package level settings for jax platform and 64 bit precision

--- src/hbmep/__init__.py
+++ src/hbmep/__init__.py
@@ -1,3 +1,16 @@
+import multiprocessing
 import logging
+
+import jax
+import numpyro
+
+PLATFORM = "cpu"
+jax.config.update("jax_platforms", PLATFORM)
+numpyro.set_platform(PLATFORM)
+
+cpu_count = multiprocessing.cpu_count() - 2
+numpyro.set_host_device_count(cpu_count)
+numpyro.enable_x64()
+numpyro.enable_validation()
 
 logger = logging.getLogger(__name__)
