40478a6673ca4acba35424c5b919b06f91f1e204
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-02-13 15:07:36 -0500

    refactor: refactor dataset class

--- src/hbmep/dataset/core.py
+++ src/hbmep/dataset/core.py
@@ -21,8 +21,7 @@ class Dataset:
         self.build_dir = config.BUILD_DIR
 
         self._features = config.FEATURES
-        self.features = []
-        self._set_features()
+        self.features = self._init_features(self._features)
         self.intensity = config.INTENSITY
         self.response = config.RESPONSE
 
@@ -35,44 +34,38 @@ class Dataset:
         self.mep_window = config.MEP_TIME_RANGE
         self.mep_size_window = config.MEP_SIZE_TIME_RANGE
 
-    def _set_features(self):
-        for feature in self._features:
+    @staticmethod
+    def _init_features(_features: list[str]) -> list[str]:
+        features = []
+        for feature in _features:
             if isinstance(feature, list):
                 feature = const.SEP.join(feature)
-            self.features.append(feature)
-        return
+            features.append(feature)
+        return features
 
-    def _make_dir(self, dir: str):
+    @staticmethod
+    def _make_dir(dir: str):
+        # TODO: try and except
         Path(dir).mkdir(parents=True, exist_ok=True)
         return
 
-    def _copy(self, src: str, dst: str):
-        try:
-            shutil.copy(src, dst)
-        except shutil.SameFileError:
-            pass
-        return
-
-    def _make_combinations(
-        self,
+    @staticmethod
+    def _get_combinations(
         df: pd.DataFrame,
         columns: list[str],
         orderby = None
     ) -> list[tuple[int]]:
-        combinations = df \
-            .groupby(by=columns) \
-            .size() \
-            .to_frame("counts") \
-            .reset_index() \
-            .copy()
-        combinations = combinations[columns] \
-            .apply(tuple, axis=1) \
+        combinations = (
+            df[columns]
+            .apply(tuple, axis=1)
+            .unique()
             .tolist()
+        )
         combinations = sorted(combinations, key=orderby)
         return combinations
 
-    def _invert_combination(
-        self,
+    @staticmethod
+    def _get_combination_inverse(
         combination: tuple[int],
         columns: list[str],
         encoder_dict: dict[str, LabelEncoder]
@@ -82,45 +75,70 @@ class Dataset:
             for (column, value) in zip(columns, combination)
         )
 
-    def _preprocess(self, df: pd.DataFrame) -> tuple[pd.DataFrame, dict[str, LabelEncoder]]:
-        """ Encode """
+    @staticmethod
+    def _preprocess(
+        df: pd.DataFrame,
+        columns: list[str]
+    ) -> tuple[pd.DataFrame, dict[str, LabelEncoder]]:
+        # Encode
         encoder_dict = defaultdict(LabelEncoder)
-        df[self.features] = \
-            df[self.features] \
-            .apply(lambda x: encoder_dict[x.name].fit_transform(x)) \
-            .copy()
-        df.reset_index(inplace=True, drop=True)
+        df[columns] = (
+            df[columns]
+            .apply(lambda x: encoder_dict[x.name].fit_transform(x))
+        )
         return df, encoder_dict
 
     @timing
-    def load(self, df: pd.DataFrame | None = None) -> tuple[pd.DataFrame, dict[str, LabelEncoder]]:
+    def load(
+        self,
+        df: pd.DataFrame | None = None
+    ) -> tuple[pd.DataFrame, dict[str, LabelEncoder]]:
         self._make_dir(dir=self.build_dir)
-        logger.info(f"Artefacts will be stored here - {self.build_dir}")
-        self._copy(src=self.toml_path, dst=self.build_dir)
-        logger.info(f"Copied config to {self.build_dir}")
+        logger.info(
+            "Artefacts will be stored here - ",
+            self.build_dir
+        )
 
+        # Read data if not provided
         if df is None:
-            csv_path = self.csv_path
-            logger.info(f"Reading data from {csv_path} ...")
-            df = pd.read_csv(csv_path)
+            logger.info(f"Reading data from {self.csv_path} ...")
+            df = pd.read_csv(self.csv_path)
 
-        """ Concatenate (necessary) features """
+        # Concatenate (necessary) features
         for i, feature in enumerate(self._features):
             if isinstance(feature, list):
-                df[self.features[i]] = df[feature].apply(
-                    lambda x: const.SEP.join(x), axis=1
+                df[self.features[i]] = (
+                    df[feature]
+                    .apply(lambda x: const.SEP.join(x), axis=1)
                 )
 
-        """ Positive response constraint """
-        num_non_positive_observation = (df[self.response] <= 0).any(axis=1).sum()
-        logger.warning(f"Total non-positive observations: {num_non_positive_observation}")
+        # Positive response constraint
+        num_non_positive_observation = (
+            (df[self.response] <= 0)
+            .any(axis=1)
+            .sum()
+        )
+        if num_non_positive_observation:
+            logger.warning(
+                "Total non-positive observations: ",
+                num_non_positive_observation
+            )
         assert not num_non_positive_observation
 
-        """ Missing response constraint """
-        num_missing_observation = df[self.response].isna().any(axis=1).sum()
-        logger.warning(f"Total missing observations: {num_missing_observation}")
+        # Missing response constraint
+        num_missing_observation = (
+            df[self.response]
+            .isna()
+            .any(axis=1)
+            .sum()
+        )
+        if num_missing_observation:
+            logger.info(
+                "Total missing observations: ",
+                num_missing_observation
+            )
         assert not num_missing_observation
 
         logger.info("Processing data ...")
-        df, encoder_dict = self._preprocess(df=df)
+        df, encoder_dict = self._preprocess(df=df, columns=self.features)
         return df, encoder_dict
