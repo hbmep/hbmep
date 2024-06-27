8c941819c974a5b1293048d6537669394c75550f
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-06-27 15:39:48 -0400

    docs: add config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,22 +1,52 @@
-import sphinx_rtd_theme
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution("hbmep").version
+except DistributionNotFound:
+    __version__ = "unknown version"
 
 
 """ Project information """
 project = 'hbmep'
-copyright = '2023, hbmep-authors'
-author = 'hbmep-authors'
-release = '0.0.1'
+copyright = '2023-2024, hbmep authors'
+version = __version__
+release = __version__
 
 """ General configuration """
-extensions = []
+extensions = [
+    'sphinxcontrib.bibtex',
+    'myst_nb',
+    'sphinx_copybutton',
+]
+
+bibtex_bibfiles = ['bibliography.bib']
+bibtex_default_style = 'unsrt'
+bibtex_reference_style = 'author_year'
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 """ Options for HTML output """
-html_theme = "sphinx_rtd_theme"
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
-html_static_path = ['_static']
+# HTML theme
+html_theme = "sphinx_book_theme"
+# html_copy_source = True
+# html_show_sourcelink = True
+# html_sourcelink_suffix = ""
+html_title = f"hbmep v{__version__}"
+# html_favicon = "_static/favicon.png"
+# html_static_path = ["_static"]
+html_theme_options = {
+    "path_to_docs": "docs",
+    "repository_url": "https://github.com/hbmep/hbmep",
+    "repository_branch": "main",
+    # "launch_buttons": {
+    #     "binderhub_url": "https://mybinder.org",
+    #     "notebook_interface": "classic",
+    # },
+    "use_edit_page_button": True,
+    "use_issues_button": True,
+    "use_repository_button": True,
+    "use_download_button": True,
+}
+nb_execution_mode = "off"
+nb_execution_timeout = -1
