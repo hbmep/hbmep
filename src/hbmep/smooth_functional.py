1a4dbc95128d6347d71cf2611be08f8615293411
vishu-tyagi <vishweshwartyagiv@gmail.com>
2024-10-24 14:17:32 -0400

    add smooth rectified-linear function

--- src/hbmep/smooth_functional.py
+++ src/hbmep/smooth_functional.py
@@ -4,31 +4,6 @@ import jax.numpy as jnp
 from hbmep import functional as F
 
 
-# def smooth_max(x, eps):
-#     """
-#     Smooth approximation of the function
-#     x \mapsto max(0, x)
-#     """
-#     z = jnp.log(
-#         2 * jnp.cosh(x / eps)
-#     )
-#     z = eps * z
-#     z = (1 / 2) * (x + z)
-#     return z
-
-
-# def smooth_max(x, eps):
-#     """
-#     Smooth approximation of the function
-#     x \mapsto max(0, x)
-#     """
-#     eps = eps * 2
-#     z = x + (
-#         eps * jnp.log1p(jnp.exp(-x / eps))
-#     )
-#     return z
-
-
 def smooth_max(x, eps):
     """
     Smooth approximation of the function
@@ -41,16 +16,6 @@ def smooth_max(x, eps):
     return z
 
 
-# def smooth_max(x, eps):
-#     """
-#     Smooth approximation of the function
-#     x \mapsto max(0, x)
-#     """
-#     eps = eps * 2
-#     z = eps * jnp.log1p(jnp.exp(x / eps))
-#     return z
-
-
 def rectified_logistic(x, a, b, L, ell, H, eps=1e-2):
     """
     Smooth approximation of rectified logistic function
@@ -59,5 +24,14 @@ def rectified_logistic(x, a, b, L, ell, H, eps=1e-2):
     z = jax.nn.sigmoid(z)
     z = (H + ell) * z
     z = -ell + z
+    z = smooth_max(z, eps)
+    return L + z
+
+
+def rectified_linear(x, a, b, L, eps=1e-2):
+    """
+    Smooth approximation of rectified linear function
+    """
+    z = F.linear_transform(x, a, b)
     z = smooth_max(z, eps)
     return L + z
