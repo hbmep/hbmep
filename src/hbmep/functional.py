2a94b7306d2629b8362da5a549888af65e6d42de
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-02-18 12:46:46 -0500

    refactor: make functional more modular

--- src/hbmep/functional.py
+++ src/hbmep/functional.py
@@ -1,66 +1,74 @@
 import jax
 import jax.numpy as jnp
-from jax.scipy.special import logit
 
 
-def linear_transform(x, a, b):
+def _linear_transform(x, a, b):
     return jnp.multiply(b, x - a)
 
 
+def _logistic_transform(x, a, b, ell, H):
+    z = _linear_transform(x, a, b) - jnp.log(H) + jnp.log(ell)
+    z = jax.nn.sigmoid(z)
+    z = (H + ell) * z
+    z = -ell + z
+    return z
+
+
+def _threshold_s50_delta(a, b, ell, H):
+    z = jnp.log(ell) - jnp.log(H + 2 * ell)
+    return z / b
+
+
 def rectified_logistic(x, a, b, L, ell, H):
-    """ This is the rectified logistic function """
-    return (
-        L
-        + jax.nn.relu(
-            - ell
-            + jnp.multiply(
-                H + ell,
-                jax.nn.sigmoid(
-                    linear_transform(x, a, b)
-                    - jnp.log(H / ell)
-                )
-            )
-        )
-    )
+    """ Rectified-logistic function in threshold parameterization """
+    z = _logistic_transform(x, a, b, ell, H)
+    z = jax.nn.relu(z)
+    return L + z
 
 
 def logistic5(x, a, b, v, L, H):
-    """ This is the logistic-5 function """
-    return (
-        L
-        + jnp.multiply(
-            H,
-            jnp.power(
-                jax.nn.sigmoid(
-                    linear_transform(x, a, b)
-                    - jnp.log(-1 + jnp.power(2, v))
-                ),
-                jnp.true_divide(1, v)
-            )
-        )
-    )
+    """ Logistic-5 function """
+    z = _linear_transform(x, a, b) - jnp.log(-1 + jnp.power(2, v))
+    z = jax.nn.sigmoid(z)
+    z = jnp.power(z, 1 / v)
+    z = H * z
+    return L + z
 
 
 def logistic4(x, a, b, L, H):
-    """ This is the logistic-4 function """
-    return (
-        L
-        + jnp.multiply(
-            H,
-            jax.nn.sigmoid(linear_transform(x, a, b))
-        )
-    )
+    """ Logistic-4 function """
+    z = _linear_transform(x, a, b)
+    z = jax.nn.sigmoid(z)
+    z = H * z
+    return L + z
 
 
 def rectified_linear(x, a, b, L):
-    """ This is the rectified linear function """
-    return (
-        L
-        + jax.nn.relu(linear_transform(x, a, b))
-    )
+    """ Rectified-linear function """
+    z = _linear_transform(x, a, b)
+    z = jax.nn.relu(z)
+    return L + z
 
 
-def prime(fn, x, *args):
+def threshold(a, b, L, ell, H):
+    """ Compute threshold of the rectified-logistic in S50 parameterization """
+    return a + _threshold_s50_delta(a, b, ell, H)
+
+
+def s50(a, b, L, ell, H):
+    """ Compute S50 of the rectified-logistic in threshold parameterization """
+    return a - _threshold_s50_delta(a, b, ell, H)
+
+
+def rectified_logistic_s50(x, a, b, L, ell, H):
+    """
+    Rectified-logistic function in S50 parameterization
+    """
+    a = threshold(a, b, L, ell, H)
+    return rectified_logistic(x, a, b, L, ell, H)
+
+
+def grad(fn, x, *args):
     """ Compute the gradient of a function """
     x = jnp.broadcast_to(x, args[0].shape)
     grad = jax.grad(fn, argnums=0)
@@ -68,41 +76,3 @@ def prime(fn, x, *args):
         grad = jax.vmap(grad)
     return grad(x, *args)
 
-
-def solve_rectified_logistic(y, a, b, L, ell, H):
-    """
-    This solves the rectified logistic function at y
-    Use this function with y = L + (H / 2) to get the S50
-    """
-    return jnp.where(
-        jnp.logical_and(y > L, y < L + H),
-        a
-        + jnp.true_divide(
-            logit(jnp.true_divide(y - L + ell, H + ell))
-            + jnp.log(jnp.true_divide(H, ell)),
-            b
-        ),
-        jnp.nan
-    )
-
-
-def rectified_logistic_S50(x, a, b, L, ell, H):
-    """
-    This is the rectified logistic function
-    in the S50 parameterization
-    """
-    return (
-        L
-        + jax.nn.relu(
-            - ell
-            + jnp.multiply(
-                H + ell,
-                jax.nn.sigmoid(
-                    linear_transform(x, a, b)
-                    - jnp.log(jnp.true_divide(
-                        H, H + jnp.multiply(2, ell)
-                    ))
-                )
-            )
-        )
-    )
--- src/hbmep/smooth_functional.py
+++ src/hbmep/smooth_functional.py
@@ -3,11 +3,12 @@ import jax.numpy as jnp
 
 from hbmep import functional as F
 
+EPS = 1e-3
 
-def smooth_max(x, eps):
+
+def _smooth_max(x, eps):
     """
-    Smooth approximation of the function
-    x \mapsto max(0, x)
+    Smooth approximation of the maximum function: x \mapsto max(0, x)
     """
     eps = eps / jnp.log(2)
     z = x + (
@@ -16,35 +17,28 @@ def smooth_max(x, eps):
     return z
 
 
-def rectified_logistic(x, a, b, L, ell, H, eps=1e-3):
+def rectified_logistic(x, a, b, L, ell, H, eps=EPS):
     """
-    Smooth approximation of rectified logistic function
+    Smooth approximation of the rectified-logistic function
     """
-    z = F.linear_transform(x, a, b) - jnp.log(H) + jnp.log(ell)
-    z = jax.nn.sigmoid(z)
-    z = (H + ell) * z
-    z = -ell + z
-    z = smooth_max(z, eps)
+    z = F._logistic_transform(x, a, b, ell, H)
+    z = _smooth_max(z, eps)
     return L + z
 
 
-def rectified_linear(x, a, b, L, eps=1e-3):
+def rectified_linear(x, a, b, L, eps=EPS):
     """
-    Smooth approximation of rectified linear function
+    Smooth approximation of the rectified-linear function
     """
-    z = F.linear_transform(x, a, b)
-    z = smooth_max(z, eps)
+    z = F._linear_transform(x, a, b)
+    z = _smooth_max(z, eps)
     return L + z
 
 
-def rectified_logistic_S50(x, a, b, L, ell, H, eps=1e-2):
+def rectified_logistic_s50(x, a, b, L, ell, H, eps=EPS):
     """
-    This is the rectified logistic function
-    in the S50 parameterization
+    Smooth approximation of rectified-logistic function in S50 parameterization
     """
-    z = F.linear_transform(x, a, b) - jnp.log(H) + jnp.log(H + 2 * ell)
-    z = jax.nn.sigmoid(z)
-    z = (H + ell) * z
-    z = -ell + z
-    z = smooth_max(z, eps)
-    return L + z
+    a = F.threshold(a, b, L, ell, H)
+    return rectified_logistic(x, a, b, L, ell, H, eps)
+
