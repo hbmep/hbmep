b8f1434b99998f7462449d874df017c28cd8382d
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-03-19 12:46:36 -0400

    rat: log divergences and tree depth, save mcmc object

--- src/hbmep/notebooks/rat/util.py
+++ src/hbmep/notebooks/rat/util.py
@@ -2,17 +2,35 @@ import os
 import pickle
 import logging 
 
-from hbmep.util import Site as site
+import numpy as np
+
+from hbmep.util import site
 from hbmep.notebooks.constants import INFERENCE_FILE, MODEL_FILE
 
 logger = logging.getLogger(__name__)
 
 
-def run(data, model):
+def run(data, model, encoder=None, **kw):
     # Run
-    df, encoder = model.load(df=data)
+    if encoder is None:
+        df, encoder = model.load(df=data)
+    else:
+        df = data.copy()
     logger.info(f"df.shape {df.shape}")
-    _, posterior = model.run(df=df)
+    mcmc, posterior = model.run(df=df, **kw)
+
+    # Save
+    output_path = os.path.join(model.build_dir, "inf.pkl")
+    with open(output_path, "wb") as f:
+        pickle.dump((df, encoder, posterior,), f)
+
+    output_path = os.path.join(model.build_dir, "model.pkl")
+    with open(output_path, "wb") as f:
+        pickle.dump((model,), f)
+
+    output_path = os.path.join(model.build_dir, "model_dict.pkl")
+    with open(output_path, "wb") as f:
+        pickle.dump((mcmc, model.__dict__,), f)
 
     # Predictions
     prediction_df = model.make_prediction_dataset(df=df)
@@ -22,32 +40,63 @@ def run(data, model):
     model.plot_curves(
         df=df,
         encoder=encoder,
-        posterior=posterior,
         prediction_df=prediction_df,
         predictive=predictive,
+        posterior=posterior,
     )
 
-    # Save
-    output_path = os.path.join(model.build_dir, INFERENCE_FILE)
-    with open(output_path, "wb") as f:
-        pickle.dump((df, encoder, posterior, model.__dict__), f)
-
-    output_path = os.path.join(model.build_dir, MODEL_FILE)
-    with open(output_path, "wb") as f:
-        pickle.dump((model,), f)
-
+    if site.outlier_prob in posterior.keys():
+        posterior.pop(site.outlier_prob)
+    summary_df = model.summary(posterior)
+    logger.info(f"Summary:\n{summary_df.to_string()}")
+    dest = os.path.join(model.build_dir, "summary.csv")
+    summary_df.to_csv(dest)
+    logger.info(f"Saved summary to {dest}")
     logger.info(f"Finished running {model.name}")
+    try:
+        divergences = mcmc.get_extra_fields()["diverging"].sum().item()
+        logger.info(f"No. of divergences {divergences}")
+        num_steps = mcmc.get_extra_fields()["num_steps"]
+        tree_depth = np.floor(np.log2(num_steps)).astype(int)
+        logger.info(f"Tree depth statistics:")
+        logger.info(f"Min: {tree_depth.min()}")
+        logger.info(f"Max: {tree_depth.max()}")
+        logger.info(f"Mean: {tree_depth.mean()}")
+    except: pass
     logger.info(f"Saved results to {model.build_dir}")
     return
 
 
 def get_subname(model):
     return (
-        f'{model.mcmc_params["num_warmup"]}W'
-        f'_{model.mcmc_params["num_samples"]}S'
-        f'_{model.mcmc_params["num_chains"]}C'
-        f'_{model.mcmc_params["thinning"]}T'
-        f'_{model.nuts_params["max_tree_depth"][0]}D'
-        f'_{model.nuts_params["target_accept_prob"] * 100:.0f}A'
-        f'_mixture{"True" if model.use_mixture else "False"}'
+        f'{model.mcmc_params["num_warmup"]}w'
+        f'_{model.mcmc_params["num_samples"]}s'
+        f'_{model.mcmc_params["num_chains"]}c'
+        f'_{model.mcmc_params["thinning"]}t'
+        f'_{model.nuts_params["max_tree_depth"][0]}d'
+        f'_{model.nuts_params["target_accept_prob"] * 100:.0f}a'
+        f'_{"t" if model.use_mixture else "f"}m'
     )
+
+
+def mask_upper(arr):
+    n = arr.shape[0]
+    arr[np.triu_indices(n)] = np.nan
+    return arr
+
+
+def annotate_heatmap(ax, cmap_arr, arr, l, r, star=False, star_arr=None, **kw):
+    n = arr.shape[0]
+    colors = np.where(cmap_arr > .8, "k", "white")
+
+    for y in range(n):
+        for x in range(n):
+            if x >= y: continue
+            text = f"{arr[y, x]}"
+            if star:
+                pvalue = star_arr[y, x]
+                if pvalue < 0.001: text += "***"
+                elif pvalue < 0.01: text += "**"
+                elif pvalue < 0.05: text += "*"
+            # ax.text(x + l, y + r, (y, x), **kw, color=colors[y, x])
+            ax.text(x + l, y + r, text, **kw, color=colors[y, x])
