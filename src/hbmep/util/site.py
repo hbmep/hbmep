5a5e7e847a842b43e29c2b2c9043242b5f7cac3f
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-03-19 12:59:41 -0400

    feat: improve Site

--- src/hbmep/util/site.py
+++ src/hbmep/util/site.py
@@ -1,43 +1,55 @@
-from dataclasses import dataclass
+class SiteAttribute(str):
+    @property
+    def raw(self):
+        return SiteAttribute(f"{self}_raw")
+
+    @property
+    def log(self):
+        return SiteAttribute(f"{self}_log")
+
+    @property
+    def loc(self):
+        return SiteAttribute(f"{self}_loc")
+
+    @property
+    def scale(self):
+        return SiteAttribute(f"{self}_scale")
+
+    def __getitem__(self, key):
+        # Handle indexing like site.num_features[0]
+        if isinstance(key, int) and key >= 0:
+            return SiteAttribute(f"{self}_{key}")
+        raise KeyError(f"Key must be a non-negative integer, got {key}")
 
 
-@dataclass
-class Site:
+class SiteMeta(type):
+    def __call__(cls, value):
+        return SiteAttribute(value)
+
+
+class Site(metaclass=SiteMeta):
     # Priors
-    a = "a"
-    b = "b"
-    g = "g"
-    h = "h"
-    v = "v"
+    a = SiteAttribute("a")
+    b = SiteAttribute("b")
+    g = SiteAttribute("g")
+    h = SiteAttribute("h")
+    v = SiteAttribute("v")
 
-    c1 = "c₁"
-    c2 = "c₂"
+    c1 = SiteAttribute("c₁")
+    c2 = SiteAttribute("c₂")
 
     # Deterministic
-    mu = "µ"
-    alpha = "α"
-    beta = "β"
+    mu = SiteAttribute("µ")
+    alpha = SiteAttribute("α")
+    beta = SiteAttribute("β")
 
     # Plates
-    num_features = [f"num_feature{i}" for i in range(10)]
-    num_response = "num_response"
-    num_data = "num_data"
-
+    num_features = SiteAttribute("num_features")
+    num_response = SiteAttribute("num_response")
+    num_data = SiteAttribute("num_data")
 
     # Observation
-    obs = "obs"
+    obs = SiteAttribute("obs")
 
     # Mixture
-    outlier_prob = "p_outlier"
-
-    @staticmethod
-    def raw(site):
-        return site + "_raw"
-
-    @staticmethod
-    def loc(site):
-        return site + "_loc"
-    
-    @staticmethod
-    def scale(site):
-        return site + "_scale"
+    outlier_prob = SiteAttribute("p_outlier")
