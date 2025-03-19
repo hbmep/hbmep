aa9aeb8219a2d2188ba587e8f4c83b8c23e9ce80
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-03-19 19:43:49 -0400

    rsch: add data, repos, reports path

--- src/hbmep/notebooks/constants.py
+++ src/hbmep/notebooks/constants.py
@@ -1,7 +1,9 @@
 import os
 
 HOME = os.getenv("HOME")
+DATA = os.path.join(HOME, "data", "hbmep-processed")
+REPOS = os.path.join(HOME, "repos", "refactor", "hbmep")
+REPORTS = os.path.join(HOME, "reports", "hbmep")
 
 MODEL_FILE = "model.pkl"
-INFERENCE_FILE = "inference.pkl"
-
+INFERENCE_FILE = "inf.pkl"
