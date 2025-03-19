5c9f2dcf5453cb60ad4c3a44d04289f46053dadf
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-03-19 12:56:18 -0400

    refactor: import util functions

--- src/hbmep/__init__.py
+++ src/hbmep/__init__.py
@@ -1,25 +1,19 @@
 import logging
 import multiprocessing
-import importlib.metadata
+from importlib.metadata import version
 
 import numpyro
 
 from hbmep.dataset import (
-    fit_transform,
-    load,
-    inverse_transform,
-    make_prediction_dataset,
+    fit_transform, load, inverse_transform, make_features, make_prediction_dataset,
 )
-from hbmep.plotter import plotter
+from hbmep.plotter import plotter, plot
+from hbmep.model import BaseModel, NonHierarchicalBaseModel
 from hbmep.infer import (
-    get_regressors,
-    get_response,
-    get_dependencies,
-    run,
-    predict,
+    get_regressors, get_response, get_dependencies, run, predict,
 )
 
-__version__ = importlib.metadata.version("hbmep")
+__version__ = version("hbmep")
 cpu_count = multiprocessing.cpu_count() - 2
 numpyro.set_host_device_count(cpu_count)
 numpyro.enable_x64()
