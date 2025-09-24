ae015d162239b55f194570d0fbc56767876e031e
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-09-24 11:01:28 -0400

    style: break long lines

--- src/hbmep/__init__.py
+++ src/hbmep/__init__.py
@@ -5,19 +5,28 @@ from importlib.metadata import version
 import numpyro
 
 from hbmep.dataset import (
-    fit_transform, load, inverse_transform, make_features, make_prediction_dataset,
+    load,
+    fit_transform,
+    inverse_transform,
+    make_features,
+    make_prediction_dataset,
 )
 from hbmep.plotter import plotter, plot
 from hbmep.model import BaseModel, NonHierarchicalBaseModel
 from hbmep.infer import (
-    get_regressors, get_response, get_dependencies, trace, run, predict,
+    get_regressors,
+    get_response,
+    get_dependencies,
+    trace,
+    run,
+    predict,
 )
 from hbmep.util import site
 
 __version__ = version("hbmep")
+logger = logging.getLogger(__name__)
+
 cpu_count = multiprocessing.cpu_count()
 numpyro.set_host_device_count(cpu_count)
 numpyro.enable_x64()
 numpyro.enable_validation()
-
-logger = logging.getLogger(__name__)
