c0e61fc770152220d045beb88b7fb73fa58d553e
vishu-tyagi <vishweshwartyagiv@gmail.com>
2025-10-02 15:52:01 -0400

    fix: remove conversion to numpy arrays

--- src/hbmep/infer.py
+++ src/hbmep/infer.py
@@ -3,7 +3,7 @@ from collections.abc import Callable
 
 import pandas as pd
 import numpy as np
-from jax import random
+from jax import random, numpy as jnp
 
 import numpyro as pyro
 from numpyro.infer import NUTS, MCMC, Predictive
@@ -14,9 +14,10 @@ logger = logging.getLogger(__name__)
 
 def get_regressors(
     df: pd.DataFrame,
+    *,
     intensity: str,
     features: list[str],
-    **kw
+    response: list[str] | None = None,
 ):
     return (
         df[[intensity]].to_numpy(),
@@ -26,8 +27,10 @@ def get_regressors(
 
 def get_response(
     df: pd.DataFrame,
+    *,
     response: list[str],
-    **kw
+    intensity: str | None = None,
+    features: list[str] | None = None,
 ):
     return df[response].to_numpy(),
 
@@ -46,30 +49,26 @@ def get_dependencies(
     )
 
 
-def trace(
-    key: random.key,
-    model: Callable,
-    intensity: np.ndarray,
-    features: np.ndarray,
-    response: np.ndarray,
-    **kw
-):
+def trace(key: random.key, model: Callable, *args, **kw):
+    r"""
+    Run model trace.
+
+    :param random.key key: Random number generator for sampling.
+    :param Callable key: Numpyro model.
+    :param args: Arguments passed to the `model`.
+    :param kw: Keyword arguments passed to the `model`. 
+    """
     with pyro.handlers.seed(rng_seed=key):
-        trace = pyro.handlers.trace(model).get_trace(
-            intensity,
-            features,
-            response,
-            **kw
-        )
+        trace = pyro.handlers.trace(model).get_trace(*args, **kw)
     return trace
 
 
 def run(
     key: random.key,
     model: Callable,
-    intensity: np.ndarray,
-    features: np.ndarray,
-    response: np.ndarray,
+    intensity: np.ndarray | jnp.ndarray,
+    features: np.ndarray | jnp.ndarray,
+    response: np.ndarray | jnp.ndarray,
     nuts_params: dict | None = None,
     mcmc_params: dict | None = None,
     mcmc: MCMC | None = None,
@@ -77,20 +76,33 @@ def run(
     init_params=None,
     **kw
 ) -> tuple[MCMC, dict]:
+    r"""
+    Run model.
+
+    :param random.key key: Random number generator for sampling.
+    :param Callable model: numpyro model.
+    :param intensity: np.ndarray | jnp.ndarray: 
+        Intensity array of shape (N, 1), N = number of observations.
+    :param features: np.ndarray | jnp.ndarray:
+        Features array of shape (N, F), F = number of feature variables.
+    :param response: np.ndarray | jnp.ndarray:
+        Response array of shape (N, R), R = number of response variables.
+    :param nuts_params: dict | None:
+        Keyword arguments passed to numpyro.infer.NUTS. Defaults to {}.
+    :param mcmc_params: dict | None:
+        Keyword arguments passed to numpyro.infer.MCMC. Defaults to {}.
+    :param extra_fields: list | tuple
+    :param init_params
+    :param kw: Keyword arguments passed to the `model`.
+    """
+    nuts_params = nuts_params or {}
+    mcmc_params = mcmc_params or {}
     if mcmc is None:
         kernel = NUTS(model, **nuts_params)
         mcmc = MCMC(kernel, **mcmc_params)
-        msg = f"Running..."
     else:
-        assert isinstance(mcmc, MCMC)
-        if mcmc.last_state is not None:
-            msg = f"Resuming from last state..."
+        if getattr(mcmc, "last_state", None) is not None:
             mcmc.post_warmup_state = mcmc.last_state
-            key = mcmc.post_warmup_state.key
-        else: msg = f"Running with provided MCMC..."
-
-    # Run
-    logger.info(msg)
     mcmc.run(
         key,
         intensity,
@@ -100,21 +112,35 @@ def run(
         init_params=init_params,
         **kw
     )
-    posterior = mcmc.get_samples()
-    posterior = {k: np.array(v) for k, v in posterior.items()}
-    return mcmc, posterior
+    return mcmc
 
 
 def predict(
     key: random.key,
     model: Callable,
-    intensity: np.ndarray,
-    features: np.ndarray,
+    intensity: np.ndarray | jnp.ndarray,
+    features: np.ndarray | jnp.ndarray,
     posterior: dict | None = None,
     num_samples: int = 100,
     return_sites: list[str] | None = None,
     **kw
 ):
+    r"""
+    Generate predictive distribution.
+
+    :param random.key key: Random number generator for sampling.
+    :param Callable model: numpyro model.
+    :param intensity: np.ndarray | jnp.ndarray: 
+        Intensity array of shape (N, 1), N = number of observations.
+    :param features: np.ndarray | jnp.ndarray:
+        Features array of shape (N, F), F = number of feature variables.
+    :param posterior: dict | None:
+        Dictionary of posterior samples.
+        Gf not provided, samples are generated from prior predictive.
+    :param extra_fields: list | tuple
+    :param init_params
+    :param kw: Keyword arguments passed to the `model`.
+    """
     if posterior is None:               # prior predictive
         predictive_fn = Predictive(
             model=model,
@@ -127,8 +153,6 @@ def predict(
             posterior_samples=posterior,
             return_sites=return_sites
         )
-
     # Generate predictions
     predictive = predictive_fn(key, intensity, features, **kw)
-    predictive = {u: np.array(v) for u, v in predictive.items()}
     return predictive
